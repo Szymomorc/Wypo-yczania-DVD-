781ab3a172af13c1be431da40e5a87345b1e7111
Szymomorc <szymon.orc@gmail.com>
2025-02-27 12:08:33 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Usunięcie wszystkich zmiennych sesyjnych
-session_destroy(); // Zniszczenie sesji
-header("Location: index.php"); // Przekierowanie na stronę główną
-exit();
-?>
