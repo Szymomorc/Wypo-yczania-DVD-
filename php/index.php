cd07f17328ccbaf2b0135f06bd31f92c403e2169
beatzuuu <szczudlo-wywrotki-5t@icloud.com>
2025-03-02 15:09:34 +0100

    dodanie styli do login/rejestracja

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@ session_start(); // Rozpoczęcie sesji
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DVD Rental</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
@@ -17,7 +17,7 @@ session_start(); // Rozpoczęcie sesji
         rel="stylesheet">
     <script src="https://kit.fontawesome.com/c876edd7c5.js" crossorigin="anonymous"></script>
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="../css/main.css">
 </head>
 
 <body class="main-page">
